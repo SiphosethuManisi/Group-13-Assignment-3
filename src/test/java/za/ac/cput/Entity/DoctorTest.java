ede1db8258eaba8ef1ecb1bbfcaed26c91f00802
Mpumelelo Magagula <218136021@mycput.ac.za>
2021-05-31 23:13:47 +0200

    Main DDM entity test

---
+++ src/test/java/za/ac/cput/Entity/DoctorTest.java
@@ -0,0 +1,12 @@
+package za.ac.cput.Entity;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DoctorTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
